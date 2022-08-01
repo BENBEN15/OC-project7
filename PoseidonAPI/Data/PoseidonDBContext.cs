29da77085f679004b6cac636c2389ae086bae556
BENBEN15 <benoit.serre@ocsalis.fr>
2022-08-01 17:14:37 +0200

    Added Bid entity in model, added Bids dbSet and according migration

--- PoseidonAPI/Data/PoseidonDBContext.cs
+++ PoseidonAPI/Data/PoseidonDBContext.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PoseidonAPI.Model;
 
 namespace PoseidonAPI.Data
 {
     public class PoseidonDBContext : IdentityDbContext<IdentityUser>
     {
         public PoseidonDBContext(DbContextOptions<PoseidonDBContext> options) : base(options) { }
+
+        public virtual DbSet<Bid> Bids { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Call Identity OnModelCreating first
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Bid>(entity =>
+            {
+                entity.HasKey(b => b.BidId);
+            });
+        }
     }
 }
---
+++ PoseidonAPI/Migrations/20220801150023_AddBidsTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoseidonAPI.Migrations
+{
+    public partial class AddBidsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bids",
+                columns: table => new
+                {
+                    BidId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Account = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BidQuantity = table.Column<double>(type: "float", nullable: true),
+                    AskQuantity = table.Column<double>(type: "float", nullable: true),
+                    BidValue = table.Column<double>(type: "float", nullable: true),
+                    Ask = table.Column<double>(type: "float", nullable: true),
+                    Benchmark = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BidDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Commentary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Security = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Trader = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Book = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    RevisionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RevisionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DealName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DealType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SourceListId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Side = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bids", x => x.BidId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bids");
+        }
+    }
+}
